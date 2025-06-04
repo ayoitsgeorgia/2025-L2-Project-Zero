4bb66bb76088be1795dad0bb19a14ec561659f8d
ayoitsgeorgia <georgiapemberton@mmc.school.nz>
2025-06-04 13:25:46 +1200

    Add files via upload

---
+++ 2025_MMF/2025_Project_Zero/2025_Project_Zero/C_01_Make_Statement_v3.py
@@ -0,0 +1,24 @@
+# Function go here
+def make_statement(statement, decoration, lines=1):
+    """Create headings (3 lines), subheadings (2 lines) and emphasise text / mini-heading (1 line). Only use emoji for
+    single line statements"""
+
+    middle = f"{decoration * 3} {statement} {decoration * 3}"
+    top_bottom = decoration * len(middle)
+
+    if lines == 1:
+        print(middle)
+    elif lines == 2:
+        print(middle)
+        print(top_bottom)
+
+    else:
+        print(top_bottom)
+        print(middle)
+        print(top_bottom)
+
+
+# Main Routine goes where
+make_statement("Programming is Fun!", "=", 3)
+make_statement("Programming is Still Fun!", "*", 2)
+make_statement("Emoji in Action!", "👍")
